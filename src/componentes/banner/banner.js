180b42ca5723ac16e68666c7acd47c6e516f5183
Erick de Brito Melo <erick.melo@sorocaba.sp.gov.br>
2023-07-09 16:03:24 -0300

    Primeiro componente: Banner

---
+++ src/componentes/banner/banner.js
@@ -0,0 +1,12 @@
+import "./banner.css"
+
+function Banner(){
+    return(
+        // a pasta public é a raiz do projeto
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="O banner principal da página do Organo" />
+        </header>        
+    )
+}
+
+export default Banner
