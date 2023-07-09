285e856c99d35be6c18190a79d11d42f0f7b3ee0
Erick de Brito Melo <erick.melo@sorocaba.sp.gov.br>
2023-07-09 17:17:31 -0300

    Componente Campo de Texto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Banner from "./componentes/Banner/Banner";
+import CampoTexto from "./componentes/CampoTexto"; // usado o index.js para encurtar o path do import e ficar mais clean
 
 function App() {
   return (
     <div className="App">
       <Banner/>
+      <CampoTexto />
     </div>
   );
 }
